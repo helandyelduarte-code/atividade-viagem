b98743ed44926128a5a16e484b63f33c423acf52
helandyelduarte-code <helandyelduarte@gmail.com>
2026-03-12 09:58:27 -0500

    com js

---
+++ style.js
@@ -0,0 +1,56 @@
+const lista = document.querySelector(".destinos") 
+
+const = paises [
+
+    {
+        pais: "paris",
+    descricao: "100+ Tours",
+    img: "assents/image.png",
+    },
+
+    
+    {
+        pais: "Singapore",
+    descricao: "300+ Tours",
+    img: "assents/singapore.png",
+    },
+
+    
+    {
+        pais: "Roma",
+    descricao: "400+ Tours",
+    img: "assents/roma.png",
+    },
+
+    
+    {
+        pais: "Bangok",
+    descricao: "100+ Tours",
+    img: "assents/bangok.png",
+    },
+
+    
+    {
+        pais: "Bali",
+    descricao: "600+ Tours",
+    img: "assents/bali.png",
+    },
+
+    {
+        pais: "Phuket",
+    descricao: "200+ Tours",
+    img: "assents/phuket.png",
+    },
+
+    {
+        pais: "Tokyo",
+    descricao: "700+ Tours",
+    img: "assents/tokyo.png",
+    },
+
+      {
+        pais: "Capadocia",
+    descricao: "900+ Tours",
+    img: "assents/capadocia.png",
+    },
+]
